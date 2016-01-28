751dc5ad6920814b6847c24bca7f3c20a5a5c32c
weeryan17 <theweeryan17@gmail.com>
2016-01-27 19:13:09 -0600

    Did some work but now i want a snack

--- Villages/src/com/weeryan17/vgs/Main.java
+++ Villages/src/com/weeryan17/vgs/Main.java
@@ -6,9 +6,13 @@ import com.weeryan17.vgs.commands.AdminCommand;
 import com.weeryan17.vgs.commands.VillageCommand;
 
 public class Main extends JavaPlugin {
+	
+	Main plugin;
+	
 	public void onEnable(){
+		plugin = this;
 		VillageCommand mainCommand = new VillageCommand();
-		AdminCommand admin = new AdminCommand();
+		AdminCommand admin = new AdminCommand(plugin);
 		mainCommand.register("admin", admin);
 		getCommand("Villages").setExecutor(mainCommand);
 		getCommand("V").setExecutor(mainCommand);
--- Villages/src/com/weeryan17/vgs/commands/AdminCommand.java
+++ Villages/src/com/weeryan17/vgs/commands/AdminCommand.java
@@ -3,24 +3,33 @@ package com.weeryan17.vgs.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import com.weeryan17.vgs.Main;
+
 import net.md_5.bungee.api.ChatColor;
 
 public class AdminCommand extends VillageCommandControler {
-
+	
+	Main instance;
+	
+	public AdminCommand(Main instance){
+		this.instance = instance;
+	}
+	
 	@Override
 	public boolean executeCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(args.length == 0){
 			sender.sendMessage(ChatColor.BLUE + "Villages Admin help");
-			return true;
-		} else if(args.length == 1){
-			String arg = args[1];
-			switch(arg){
-			case "test":{
-				sender.sendMessage("Test");
+		} else{
+			switch(args[0]){
+			case "toggle":{
+				sender.sendMessage("Toggle test");
 			}
-			}	
+			case "test":{
+				sender.sendMessage("Test test");
+			}
+			
+			}
 		}
-		sender.sendMessage("No such command or argument");
 		return false;
 	}
 	
