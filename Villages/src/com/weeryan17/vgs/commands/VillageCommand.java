2baf6661456352a7bc3ebccf64a36ae3059d0591
weeryan17 <theweeryan17@gmail.com>
2016-02-04 20:16:26 -0600

    Trying a 1 class command system

--- Villages/src/com/weeryan17/vgs/commands/VillageCommand.java
+++ Villages/src/com/weeryan17/vgs/commands/VillageCommand.java
@@ -1,19 +1,24 @@
 package com.weeryan17.vgs.commands;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import net.md_5.bungee.api.ChatColor;
-
-public class VillageCommand extends VillageCommandControler {
+public class VillageCommand implements CommandExecutor {
 
 	@Override
-	public boolean executeCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(args.length == 0){
-			sender.sendMessage(ChatColor.BLUE + "Village command help");
-			return true;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(args.length >= 1){
+			switch(args[0]){
+			case "help":{
+				
+			}
+			}
+		} else {
+			
 		}
-		sender.sendMessage("No such command or argument");
+		
 		return false;
 	}
 
