5eac6bdce29e7e69b473a7f313fafc5182cb0952
weeryan17 <theweeryan17@gmail.com>
2016-02-05 11:53:12 -0600

    I hate errors

--- Villages/src/com/weeryan17/vgs/Main.java
+++ Villages/src/com/weeryan17/vgs/Main.java
@@ -2,7 +2,6 @@ package com.weeryan17.vgs;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.weeryan17.vgs.commands.AdminCommand;
 import com.weeryan17.vgs.commands.VillageCommand;
 
 public class Main extends JavaPlugin {
@@ -12,8 +11,6 @@ public class Main extends JavaPlugin {
 	public void onEnable(){
 		plugin = this;
 		VillageCommand mainCommand = new VillageCommand();
-		AdminCommand admin = new AdminCommand(plugin);
-		mainCommand.register("admin", admin);
 		getCommand("Villages").setExecutor(mainCommand);
 		getCommand("V").setExecutor(mainCommand);
 	}
