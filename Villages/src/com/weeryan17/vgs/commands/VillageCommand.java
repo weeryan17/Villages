2074d00860d5214563f72262314272ab61085cc7
weeryan17 <theweeryan17@gmail.com>
2016-02-12 12:19:29 -0600

    Added in player util.
    This is for doing stuff with players that i want a nice method to

--- Villages/src/com/weeryan17/vgs/commands/VillageCommand.java
+++ Villages/src/com/weeryan17/vgs/commands/VillageCommand.java
@@ -4,6 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.weeryan17.vgs.util.PlayerUtil;
 
 public class VillageCommand implements CommandExecutor {
 
@@ -27,15 +30,26 @@ public class VillageCommand implements CommandExecutor {
 		return false;
 	}
 	public void help(CommandSender sender, String command){
+		
 		sender.sendMessage(ChatColor.YELLOW + "Villages help");
 		sender.sendMessage(ChatColor.YELLOW + "Oo--------------------------------oO");
 		switch(command){
 		case "help":{
-			sender.sendMessage(ChatColor.DARK_GREEN + "/help: " + ChatColor.BLUE + "This is the command to show you the help menu");
+			sender.sendMessage(ChatColor.DARK_GREEN + "/v help: " + ChatColor.BLUE + "This is the command to show you the help menu");
 		}
 		case "general":{
-			
-		}
+			sender.sendMessage(ChatColor.DARK_GREEN + "/v help: " + ChatColor.BLUE + "This is the command for showing this help menu (or you chould just so /v)");
+			if(sender.hasPermission("villages.admin")){
+				sender.sendMessage(ChatColor.DARK_GREEN + "/v admin: " + ChatColor.BLUE + "This is the command for the admin help menu. It only shows commands you have access to.");
+			}
+			if(sender instanceof Player){
+				PlayerUtil p = (PlayerUtil) sender;
+				sender.sendMessage(ChatColor.DARK_GREEN + "/v new [name]: " + ChatColor.BLUE + "Creates a new village with the specifyed name");
+				if(p.checkVillageOwner()){
+					sender.sendMessage(ChatColor.DARK_GREEN + "/v options: " + ChatColor.BLUE + "Brings up the town options gui.");
+				}
+				}
+			}
 		}
 	}
 
---
+++ Villages/src/com/weeryan17/vgs/util/PlayerUtil.java
@@ -0,0 +1,10 @@
+package com.weeryan17.vgs.util;
+
+import org.bukkit.entity.Player;
+
+public abstract class PlayerUtil implements Player {
+	public boolean checkVillageOwner(){
+		
+		return false;
+	}
+}
